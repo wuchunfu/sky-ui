9cee30a1f68ba76c5faa2b72e2df3f0a3264a4a8
lanyulei <934744141@qq.com>
2022-02-19 00:57:55 +0800

    feat: 添加表单设计器。

---
+++ src/components/formGenerator/core/element/ElCustomHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-header class="btn-bar">
+    <slot></slot>
+    <el-button
+      v-if="$attrs.uploadJson"
+      type="text"
+      size="medium"
+      @click="$emit('uploadJson')"
+			icon='Upload'
+    >
+      导入JSON
+    </el-button>
+    <el-button
+      v-if="$attrs.clearable"
+      type="text"
+      size="medium"
+      @click="$emit('clearable')"
+			icon='Delete'
+    >
+      清空
+    </el-button>
+    <el-button
+      v-if="$attrs.preview"
+      type="text"
+      size="medium"
+      @click="$emit('preview')"
+			icon='View'
+    >
+      预览
+    </el-button>
+    <el-button
+      v-if="$attrs.generateJson"
+      type="text"
+      size="medium"
+      @click="$emit('generateJson')"
+			icon='Tickets'
+    >
+      生成JSON
+    </el-button>
+    <el-button
+      v-if="$attrs.generateCode"
+      type="text"
+      size="medium"
+      @click="$emit('generateCode')"
+			icon='DocumentCopy'
+    >
+      生成代码
+    </el-button>
+  </el-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SvgIcon from '/@/components/svgIcon/index.vue';
+
+export default defineComponent({
+  name: 'CustomHeader',
+  components: {
+		SvgIcon
+  },
+  emits: ['uploadJson', 'clearable', 'preview', 'generateJson', 'generateCode']
+})
+</script>
---
+++ src/components/formGenerator/enums/index.ts
@@ -0,0 +1,13 @@
+export enum CodeType {
+  // eslint-disable-next-line no-unused-vars
+  Vue = 'vue',
+  // eslint-disable-next-line no-unused-vars
+  Html = 'html'
+}
+
+export enum PlatformType {
+  // eslint-disable-next-line no-unused-vars
+  Antd,
+  // eslint-disable-next-line no-unused-vars
+  Element
+}
